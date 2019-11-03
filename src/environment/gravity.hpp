e22d2e2f9e7c939dd79dcc87f890655f9cb95fe5
sksat <sksat@sksat.net>
2019-11-03 19:11:21 +0900

    [add] first gravity model

---
+++ src/environment/gravity.hpp
@@ -0,0 +1,36 @@
+#ifndef ENVIRONMENT_GRAVITY_HPP_
+#define ENVIRONMENT_GRAVITY_HPP_
+
+#include "../math.hpp"
+
+namespace environment {
+	namespace constant {
+		// 2018 CODATA recommended values
+		constexpr math::Float G		= 6.6743015e-11;
+
+		// standard acceleration of gravity(definition)
+		constexpr math::Float gn	= 9.80665;
+
+		// Chronological Scientific Tables 2014
+		constexpr math::Float re	= 6.3781366e6;
+	}
+
+	// TODO
+	// latitude model(Helmert equation, WGS-84)
+	// gravitation model
+	// other planet
+
+	template<typename T>
+	inline auto gravity(const T &pos) -> const T {
+		using namespace constant;
+
+		const auto tmp = re / (re + pos.altitude());
+		const auto g = gn * tmp * tmp;
+
+		T ret;
+		ret.altitude(-1.0 * g);
+		return ret;
+	}
+}
+
+#endif
